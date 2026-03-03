a6ecfd7adce72e611dab2c740d3c13254fa3bd6e
Lucian Tudora <ID+username@users.noreply.github.com>
2026-03-03 20:15:35 +0200

    123

--- custom_components/climate_react/switch.py
+++ custom_components/climate_react/switch.py
@@ -7,6 +7,7 @@ from typing import Any
 
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import STATE_UNAVAILABLE, STATE_UNKNOWN
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
@@ -56,6 +57,16 @@ class ClimateReactSwitch(SwitchEntity):
     def is_on(self) -> bool:
         """Return true if Climate React is enabled."""
         return self._controller.enabled
+
+    @property
+    def available(self) -> bool:
+        """Return true if the climate entity is available."""
+        climate_state = self._controller.hass.states.get(
+            self._controller.climate_entity
+        )
+        if not climate_state:
+            return False
+        return climate_state.state not in (STATE_UNAVAILABLE, STATE_UNKNOWN)
 
     @property
     def icon(self) -> str:
